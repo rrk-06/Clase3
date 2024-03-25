c1aaec58ff6cc6fde88eb738944526a3977c6f3d
alumnos <alumnos@192.168.2.71>
2024-03-25 11:35:40 -0300

    mover

--- src/geometria/Circulo.java
+++ src/geometria/Circulo.java
@@ -11,4 +11,18 @@ public class Circulo {
 		this.radio = radio;
 		this.centro = new Punto(xc,yc);
 	}
+	
+	public void mover(double enX, double enY) {
+		centro.mover(enX, enY);
+	}
+	
+	@Override
+	public String toString() {
+		return "Circulo [radio=" + radio + ", centro=" + centro + "]";
+	}
+	
+	public static void main(String[] args) {
+		Circulo si = new Circulo(5,2,4);
+		System.out.println(si);
+	}
 }
